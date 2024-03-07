54a04d7fa37afff1130a521f649b9e1b2817ab3c
Andros Fenollosa <tanrax@users.noreply.github.com>
2024-03-07 09:41:50 +0100

    Update page_controller.js

--- js/controllers/page_controller.js
+++ js/controllers/page_controller.js
@@ -6,11 +6,16 @@ export default class extends Controller {
 
   changePage(event) {
     event.preventDefault();
-    const data = Object.assign({},
-      event.currentTarget.dataset.scroll
-        ? {"scroll": event.currentTarget.dataset.scroll}
-        : {}
-    );
+    // Add all data-* attributes to the data object
+    let data = {};
+    const omitKeys = ["action", "controller", "page"];
+    for (const [key, value] of Object.entries(event.currentTarget.dataset)) {
+      // Omit omitKeys
+      if (!omitKeys.includes(key)) {
+        data[key] = value;
+      }
+    }
+    // Send the data to the server
     sendData({action: `${event.currentTarget.dataset.page}->send_page`, data: data});
   };
 }
