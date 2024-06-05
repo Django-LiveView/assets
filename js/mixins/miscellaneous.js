c99664dc7b732549a81f5fce7a8833fd82012fb5
Andros Fenollosa <tanrax@users.noreply.github.com>
2024-06-05 14:05:54 +0200

    Update miscellaneous.js

--- js/mixins/miscellaneous.js
+++ js/mixins/miscellaneous.js
@@ -33,27 +33,26 @@ function scrollToTop () {
 
 // Renders the HTML received from the Consumer or from indexedDB
 export const renderHTML = (data) => {
-
   const targetHTML = document.querySelector(data.selector);
   if (targetHTML) {
     // Remove the content of the target
     if (data.remove) {
         targetHTML.remove();
     } else {
+        const reScript = /<script\b[^>]*>([\s\S]*?)<\/script>/gm;
+        const htmlText = data.html.replace(reScript, '');
         if (data.append) {
-            const reScript = /<script\b[^>]*>([\s\S]*?)<\/script>/gm;
-            // Add HTML to the target
-            const htmlText = data.html.replace(reScript, '');
+            // Add the content to the target
             targetHTML.insertAdjacentHTML("beforeend", htmlText);
-            // Add JS to the target
-            for (const match of data.html.matchAll(reScript)) {
-                const script = document.createElement('script');
-                script.textContent = match[1];
-                targetHTML.insertAdjacentElement("afterend", script);
-            }
         } else {
             // Replace the content of the target
             targetHTML.innerHTML = data.html;
+        }
+        // Add JS to the target
+        for (const match of data.html.matchAll(reScript)) {
+            const script = document.createElement('script');
+            script.textContent = match[1];
+            targetHTML.insertAdjacentElement("beforeend", script);
         }
         // If it is a new page or is backward, the scroll returns to the beginning
         if ( data.html && !data.scroll && data.url) {
